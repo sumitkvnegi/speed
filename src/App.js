54d4fd29b73cc71cc279db71506c7160a4c7b3fa
Sumit negi <69234191+sumitkvnegi@users.noreply.github.com>
2022-12-10 17:47:00 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,8 +1,8 @@
 import React, { useState } from 'react'
 import './App.css'
-import car from './assets/1.png'
-import w1 from './assets/2.png'
-import w2 from './assets/3.png'
+// import car from './assets/1.png'
+// import w1 from './assets/2.png'
+// import w2 from './assets/3.png'
 
 const App = () => {
   const [distance, setDistance] = useState(1);
@@ -44,14 +44,14 @@ const App = () => {
         <h1 style={speed && isFinite(speed) ? {opacity:"1"} : {opacity: "0"}}>{speed <= 60 ? `Slow ${speed} km/hr.` : speed >60 && speed < 100 ? `Moderate ${speed} km/hr.` : speed >= 100 ? `Fast ${speed} km/hr.` : `${' '}`}</h1>
         <h1 style={speed?{display:"none"}:{display:"block", opacity:0}}>.</h1>
       </div>
-      <div className='container'>
-        <div className='parent' style={speed ? {animation: `run infinite ${100/speed}s linear`} : {}} >
+//       <div className='container'>
+//         <div className='parent' style={speed ? {animation: `run infinite ${100/speed}s linear`} : {}} >
 
-          <img width="150px" height="150px" src={car} alt='car' />
-          <img style={speed ? spin : {}} src={w1} alt='wheel1' />
-          <img style={speed ? spin : {}} src={w2} alt='wheel2' />
-        </div>
-      </div>
+//           <img width="150px" height="150px" src={car} alt='car' />
+//           <img style={speed ? spin : {}} src={w1} alt='wheel1' />
+//           <img style={speed ? spin : {}} src={w2} alt='wheel2' />
+//         </div>
+//       </div>
     </div>
   )
 }
